ac8d47904803230f7ef5038beaa698d8dc5970f7
Karine Gantin <k.gantin.kg@gmail.com>
2017-09-10 08:50:36 +0200

    hello fellow JHU reviewers

---
+++ plot1.R
@@ -0,0 +1,52 @@
+
+##################RETRIEVING AND PREPARING THE DATA ################################
+
+##Dataset names
+
+
+
+my_file <-"household_power_consumption.txt"
+HPC_Dataset <- "household_power_consumption.zip"
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+
+## Download, unzip, and prepare the dataset if not already there:
+
+if (!exists("hpc_data2")){
+        
+        if (!file.exists("household_power_consumption.txt")) {
+                if (!file.exists(HPC_Dataset)){
+                        download.file(fileURL, HPC_Dataset)
+                }
+                
+                
+                
+        }
+        my_file <- unzip(HPC_Dataset)
+        my_table <- read.table(my_file, header = TRUE, sep = ";", stringsAsFactors = FALSE)
+        
+        ##subsetting 
+        my_data <- subset(my_table, my_table$Date == "1/2/2007" | my_table$Date == "2/2/2007")
+        rm(my_table)
+        
+        ## merging & converting Date & Time to appropriate format (2 columns -> 1 column)
+        TDate <- paste(my_data$Date, my_data$Time)
+        hpc_data2 <- cbind(TDate, my_data[,3:9])
+        hpc_data2$TDate <- strptime(hpc_data2$TDate, format = "%d/%m/%Y %H:%M:%S")
+        
+        ## converting character to numbers in other variable columns
+        hpc_data2$Global_active_power <- as.numeric(hpc_data2$Global_active_power)
+        hpc_data2$Global_reactive_power <- as.numeric(hpc_data2$Global_reactive_power)
+        
+        ## removing unnecessary files
+        rm(HPC_Dataset, fileURL, my_file)
+        
+}
+
+
+
+
+##################### PLOT 1 : GLOBAL ACTIVE POWER #################################################
+png(filename = "plot1.png")
+hist(hpc_data2$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,53 @@
+
+##################RETRIEVING AND PREPARING THE DATA ################################
+
+##Dataset names
+
+
+
+my_file <-"household_power_consumption.txt"
+HPC_Dataset <- "household_power_consumption.zip"
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+
+## Download, unzip, and prepare the dataset if not already there:
+
+if (!exists("hpc_data2")){
+        
+        if (!file.exists("household_power_consumption.txt")) {
+                if (!file.exists(HPC_Dataset)){
+                        download.file(fileURL, HPC_Dataset)
+                }
+                
+                
+                
+        }
+        my_file <- unzip(HPC_Dataset)
+        my_table <- read.table(my_file, header = TRUE, sep = ";", stringsAsFactors = FALSE)
+        
+        ##subsetting 
+        my_data <- subset(my_table, my_table$Date == "1/2/2007" | my_table$Date == "2/2/2007")
+        rm(my_table)
+        
+        ## merging & converting Date & Time to appropriate format (2 columns -> 1 column)
+        TDate <- paste(my_data$Date, my_data$Time)
+        hpc_data2 <- cbind(TDate, my_data[,3:9])
+        hpc_data2$TDate <- strptime(hpc_data2$TDate, format = "%d/%m/%Y %H:%M:%S")
+        
+        ## converting character to numbers in other variable columns
+        hpc_data2$Global_active_power <- as.numeric(hpc_data2$Global_active_power)
+        hpc_data2$Global_reactive_power <- as.numeric(hpc_data2$Global_reactive_power)
+        
+        ## removing unnecessary files
+        rm(HPC_Dataset, fileURL, my_file)
+        
+}
+
+##################### PLOT 2 : Global active power along time ######################################
+
+png(filename = "plot2.png")
+Sys.setlocale("LC_TIME", "English") 
+plot(hpc_data2$TDate, hpc_data2$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "", type = "n")
+lines(hpc_data2$TDate, hpc_data2$Global_active_power, type = "l")
+dev.off()
+
